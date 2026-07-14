350922cfc6d635dd5d37bc96f97e6c10e117826b
Nosa <osaghaenosa2001@gmail.com>
2026-07-14 13:14:49 +0100

    fixed some cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ connectDB();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'https://funmi-aesthetics.vercel.app',
     credentials: true,
   })
 );
